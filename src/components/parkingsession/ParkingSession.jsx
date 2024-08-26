d53ee2bc2ae35b531f56f7b6d720da0a5d601c9a
SS-93 <Sesughtor@gmail.com>
2024-08-26 01:52:32 -0700

    OCR and DOCUMENT VIEWING NOW AVAILABLE FOR BULK UPLOAD

--- src/components/parkingsession/ParkingSession.jsx
+++ src/components/parkingsession/ParkingSession.jsx
@@ -7,12 +7,12 @@ import { useParams } from 'react-router-dom';
 import TextModule from '../textmodule/TextModule';
 
 function ParkingSession() {
-    const { parkingSessionId } = useParams();
+    const { parkingSessionId, parkId } = useParams();
     const [documents, setDocuments] = useState([]);
     const [selectedDocumentUrl, setSelectedDocumentUrl] = useState('');
     const [isAddingDocuments, setIsAddingDocuments] = useState(false);
     const [uploadedDocuments, setUploadedDocuments] = useState([]);
- 
+    const [ocrText, setOcrText] = useState("");
 
     
     
@@ -34,20 +34,51 @@ function ParkingSession() {
         }
       };
 
-    const handleDocumentView = (url) => {
+      useEffect(() => {
+        fetchDocuments(); // Fetch documents when component mounts
+      }, [parkingSessionId]);
+    
+      const fetchDocuments = async () => {
+        try {
+          const response = await fetch(`http://localhost:4000/dms/parked-uploads/:parkId`);
+          if (response.ok) {
+            const result = await response.json();
+            setDocuments(result.files); // Update state with fetched documents
+          } else {
+            console.error('Failed to fetch documents');
+          }
+        } catch (error) {
+          console.error('Error fetching documents:', error);
+        }
+      };
+    
+      const handleDocumentView = (url) => {
         setSelectedDocumentUrl(url);
-    };
+      };
+    
+      const handleCloseDocumentViewer = () => {
+        setSelectedDocumentUrl('');
+      };
+    
+      const handleReadDocument = (text) => {
+        setOcrText(text);
+      };
+    
 
     return (
         <div className="parking-session-container">
 
-            <DocumentViewer/>
-            <TextModule/>
+            <DocumentViewer
+               documentUrl={selectedDocumentUrl}
+               onClose={handleCloseDocumentViewer}/>
+            <TextModule text={ocrText} 
+          documentUrl = {selectedDocumentUrl}/>
             <DocumentDashboard
                 documents={documents}
                 parkingSessionId={parkingSessionId}
                 isAddingDocuments={isAddingDocuments}
                 setIsAddingDocuments={setIsAddingDocuments}
+                onViewDocument={handleDocumentView} // Pass the handler to DocumentDashboard
             />
             <AgnosticInterface
                 documents={documents}
